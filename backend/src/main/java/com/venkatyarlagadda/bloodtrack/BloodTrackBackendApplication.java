1e4b1788c03b8919e3983dcf6c0f44d1d47ee3d9
Venkat Yarlagadda <Venkat.yarl@gmail.com>
2025-10-18 20:20:05 -0400

    Initial Backend project download from start.spring.io

---
+++ backend/src/main/java/com/venkatyarlagadda/bloodtrack/BloodTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.venkatyarlagadda.bloodtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodTrackBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/venkatyarlagadda/bloodtrack/BloodTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatyarlagadda.bloodtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
